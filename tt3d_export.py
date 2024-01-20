54d00c649e8e996ab937294a1fa4ab758d52f23c
Matteo Conti <contimatteo1505@gmail.com>
2024-01-20 18:55:00 +0100

    WIP

--- tt3d_export.py
+++ tt3d_export.py
@@ -1,18 +1,13 @@
-from typing import Tuple, Dict, Iterator, Any
+from typing import Tuple, Iterator, Any
+from pathlib import Path
 
-import argparse
 import torch
 
-from pathlib import Path
 from shap_e.models.download import load_model
-from shap_e.util.notebooks import decode_latent_images
 from shap_e.util.notebooks import decode_latent_mesh
-
 from utils import Utils
 
 ###
-
-T_Latents = Tuple[str, torch.Tensor]
 
 device = Utils.Cuda.init()
 
@@ -24,7 +19,7 @@ def _load_models() -> Any:
     return xm
 
 
-def _load_latents() -> Iterator[T_Latents]:
+def _load_latents() -> Iterator[Tuple[str, torch.Tensor]]:
     source_path = Path("outputs", "latents")
 
     print("")
@@ -33,7 +28,6 @@ def _load_latents() -> Iterator[T_Latents]:
             filename = "latents.pt"
             filepath = prompt_path.joinpath(filename)
             print(prompt_path.name)
-            print(filepath.exists(), filepath)
             assert filepath.exists() and filepath.is_file()
             prompt = prompt_path.name.replace("_", " ")
             yield prompt, torch.load(filepath)
@@ -66,29 +60,19 @@ def _convert_latents_to_objs(xm_model: Any) -> None:
 ###
 
 
-def main(source_path: Path, out_path: Path) -> None:
+def main() -> None:
     xm_model = _load_models()
-    _convert_latents_to_objs(
-        xm_model=xm_model,
-        source_path=source_path,
-        out_path=out_path,
-    )
+    _convert_latents_to_objs(xm_model=xm_model)
 
 
 ###
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser()
+    # parser = argparse.ArgumentParser()
     # parser.add_argument('--prompt', type=str, required=True)
-    parser.add_argument('--source-path', type=Path, required=True)
-    parser.add_argument('--out-path', type=Path, required=True)
-
-    args = parser.parse_args()
+    # args = parser.parse_args()
 
     #
 
-    main(
-        source_path=args.source_path,
-        out_path=args.out_path,
-    )
+    main()
