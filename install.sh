e6998d8b3c75347ca7cbf6f18c206f70dff626bb
Matteo Conti <contimatteo1505@gmail.com>
2024-01-22 15:47:16 +0100

    WIP

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pip install -U pip wheel
+pip install torch --index-url https://download.pytorch.org/whl/cu118
+# pip install torch --index-url https://download.pytorch.org/whl/cu121
+pip install -e modules/shap-e/
+pip install pyyaml ipywidgets
