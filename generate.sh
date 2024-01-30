d653cb0eb8f78d256ff078230d5bf18d511e0231
Matteo Conti <contimatteo1505@gmail.com>
2024-01-30 22:50:02 +0100

    WIP

--- generate.sh
+++ generate.sh
@@ -4,7 +4,7 @@
 
 exit 0
 
-CUDA_VISIBLE_DEVICES=3 python3 tt3d_generate.py \
+CUDA_VISIBLE_DEVICES=2 python3 tt3d_generate.py \
   --prompt-file /media/data2/mconti/TT3D/prompts/test.v1.n2.txt \
   --out-path /media/data2/mconti/TT3D/models/ShapE/outputs/
 # --skip-existing
