64d76a1645448e627e64011ffe917373e20d3e5a
Matteo Conti <contimatteo1505@gmail.com>
2024-02-05 18:49:18 +0100

    WIP

--- export.sh
+++ export.sh
@@ -4,5 +4,5 @@
 
 exit 0
 
-CUDA_VISIBLE_DEVICES=2 python3 tt3d_export.py \
-  --source-path /media/data2/mconti/TT3D/models/ShapE/outputs/
+CUDA_VISIBLE_DEVICES=1 python3 tt3d_export.py \
+  --source-path /media/data2/mconti/TT3D/outputs/ShapE/
--- generate.sh
+++ generate.sh
@@ -4,7 +4,7 @@
 
 exit 0
 
-CUDA_VISIBLE_DEVICES=2 python3 tt3d_generate.py \
-  --prompt-file /media/data2/mconti/TT3D/prompts/test.v1.n2.txt \
-  --out-path /media/data2/mconti/TT3D/models/ShapE/outputs/
+CUDA_VISIBLE_DEVICES=1 python3 tt3d_generate.py \
+  --prompt-file /media/data2/mconti/TT3D/prompts/test.t3bench.n1.txt \
+  --out-path /media/data2/mconti/TT3D/outputs/ShapE/
 # --skip-existing
